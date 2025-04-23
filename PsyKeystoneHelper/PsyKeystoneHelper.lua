95be8cb13deb46cc4dc6d060b878f6ae80d24a39
Callum Vernon <clvernon1997@gmail.com>
2025-04-23 21:48:53 +0100

    Refactor out chat commands to separate file

--- PsyKeystoneHelper/PsyKeystoneHelper.lua
+++ PsyKeystoneHelper/PsyKeystoneHelper.lua
@@ -14,7 +14,7 @@ PsyKeystoneHelperDBI = LibStub("LibDataBroker-1.1"):NewDataObject("PsyKeystoneHe
         if buttonPressed == "RightButton" then
             PsyKeystoneHelper:toggleSessionStatus()
         elseif buttonPressed == "MiddleButton" then
-            PsyKeystoneHelper:handleChatCommand("commands")
+            PsyKeystoneHelper:printChatCommands()
         elseif buttonPressed == "LeftButton" then
             if PsyKeystoneHelper.frame:IsShown() then
                 PsyKeystoneHelper.frame:Hide()
@@ -116,68 +116,6 @@ end
 --------------------------------------------------------------------------------------------------------------------------------------------
 -- Chat Commands
 --------------------------------------------------------------------------------------------------------------------------------------------
-
-function PsyKeystoneHelper:handleChatCommand(input)
-    local args = { strsplit(' ', input) }
-
-    for _, arg in ipairs(args) do
-        if arg == "session" then
-            PsyKeystoneHelper:toggleSessionStatus()
-            return
-        elseif arg == "show" or arg == "" then
-            if PsyKeystoneHelper.frame:IsShown() then
-                PsyKeystoneHelper.frame:Hide()
-            else
-                PsyKeystoneHelper.frame:Show()
-            end
-            return
-        elseif arg == "request" then
-            PsyKeystoneHelper:requestInformation()
-            return
-        elseif arg == "send" then
-            PsyKeystoneHelper:sendInformation()
-            return
-        elseif arg == "cache" then
-            DevTools_Dump(PsyKeystoneHelper.db.profile.keystoneCache)
-            return
-        elseif arg == "clear" then
-            PsyKeystoneHelper.db.profile.keystoneCache = {}
-            PsyKeystoneHelper:Print("Cache cleared")
-            ns:renderData()
-            return
-        elseif arg == "version" then
-            PsyKeystoneHelper:Print("Current Version: " .. PsyKeystoneHelper.v)
-            if PsyKeystoneHelper:getSessionStatus() then
-                PsyKeystoneHelper:Print("Received Player Versions: ")
-                for _, playerData in pairs(PsyKeystoneHelper.db.profile.keystoneCache) do
-                    PsyKeystoneHelper:Print(playerData.name .. " - " .. (playerData.version or "Unknown"))
-                end
-            end
-            return
-        elseif arg == "debug" then
-            if PsyKeystoneHelper.db.profile.debugMode then
-                PsyKeystoneHelper.db.profile.debugMode = false
-                PsyKeystoneHelper:Print("Debug mode is: |cffffff33Disabled|r")
-            else
-                PsyKeystoneHelper.db.profile.debugMode = true
-                PsyKeystoneHelper:Print("Debug mode is: |cffffff33Enabled|r")
-            end
-            ns:renderData()
-            return
-        elseif arg == "commands" or arg == "command" or arg == "help" or arg == "?" then
-        else
-            PsyKeystoneHelper:Print("Unknown command")
-        end
-    end
-
-    PsyKeystoneHelper:Print("Chat Commands:")
-    PsyKeystoneHelper:Print("|cffffaeae/pkh|r " .. "|cffffff33session|r " .. "- Toggle the state of the session")
-    PsyKeystoneHelper:Print("|cffffaeae/pkh|r " .. "|cffffff33show|r " .. "- Show the Keystone Helper window")
-    PsyKeystoneHelper:Print("|cffffaeae/pkh|r " .. "|cffffff33request|r " .. "- Request data from the party")
-    PsyKeystoneHelper:Print("|cffffaeae/pkh|r " .. "|cffffff33send|r " .. "- Send data to the party")
-    PsyKeystoneHelper:Print("|cffffaeae/pkh|r " .. "|cffffff33version|r " .. "- Show version information")
-    PsyKeystoneHelper:Print("|cffffaeae/pkh|r " .. "|cffffff33clear|r " .. "- Clear the cache data")
-end
 
 function PsyKeystoneHelper:DebugPrint(msg)
     if PsyKeystoneHelper.db == nil or PsyKeystoneHelper.db.profile == nil then
---
+++ PsyKeystoneHelper/func/ChatCommands.lua
@@ -0,0 +1,71 @@
+local _, ns = ...
+
+local PsyKeystoneHelper = ns.PsyKeystoneHelper
+
+function PsyKeystoneHelper:printChatCommands()
+    PsyKeystoneHelper:Print("Chat Commands:")
+    PsyKeystoneHelper:Print("|cffffaeae/pkh|r " .. "|cffffff33session|r " .. "- Toggle the state of the session")
+    PsyKeystoneHelper:Print("|cffffaeae/pkh|r " .. "|cffffff33show|r " .. "- Show the Keystone Helper window")
+    PsyKeystoneHelper:Print("|cffffaeae/pkh|r " .. "|cffffff33request|r " .. "- Request data from the party")
+    PsyKeystoneHelper:Print("|cffffaeae/pkh|r " .. "|cffffff33send|r " .. "- Send data to the party")
+    PsyKeystoneHelper:Print("|cffffaeae/pkh|r " .. "|cffffff33version|r " .. "- Show version information")
+    PsyKeystoneHelper:Print("|cffffaeae/pkh|r " .. "|cffffff33clear|r " .. "- Clear the cache data")
+end
+
+function PsyKeystoneHelper:handleChatCommand(input)
+    local args = { strsplit(' ', input) }
+
+    for _, arg in ipairs(args) do
+        if arg == "session" then
+            PsyKeystoneHelper:toggleSessionStatus()
+            return
+        elseif arg == "show" or arg == "" then
+            if PsyKeystoneHelper.frame:IsShown() then
+                PsyKeystoneHelper.frame:Hide()
+            else
+                PsyKeystoneHelper.frame:Show()
+            end
+            return
+        elseif arg == "request" then
+            PsyKeystoneHelper:requestInformation()
+            return
+        elseif arg == "send" then
+            PsyKeystoneHelper:sendInformation()
+            return
+        elseif arg == "cache" then
+            DevTools_Dump(PsyKeystoneHelper.db.profile.keystoneCache)
+            return
+        elseif arg == "clear" then
+            PsyKeystoneHelper.db.profile.keystoneCache = {}
+            PsyKeystoneHelper:Print("Cache cleared")
+            ns:renderData()
+            return
+        elseif arg == "version" then
+            PsyKeystoneHelper:Print("Current Version: " .. PsyKeystoneHelper.v)
+            if PsyKeystoneHelper:getSessionStatus() then
+                PsyKeystoneHelper:Print("Received Player Versions: ")
+                for _, playerData in pairs(PsyKeystoneHelper.db.profile.keystoneCache) do
+                    PsyKeystoneHelper:Print(playerData.name .. " - " .. (playerData.version or "Unknown"))
+                end
+            end
+            return
+        elseif arg == "debug" then
+            if PsyKeystoneHelper.db.profile.debugMode then
+                PsyKeystoneHelper.db.profile.debugMode = false
+                PsyKeystoneHelper:Print("Debug mode is: |cffffff33Disabled|r")
+            else
+                PsyKeystoneHelper.db.profile.debugMode = true
+                PsyKeystoneHelper:Print("Debug mode is: |cffffff33Enabled|r")
+            end
+            ns:renderData()
+            return
+        elseif arg == "commands" or arg == "command" or arg == "help" or arg == "?" then
+            PsyKeystoneHelper:printChatCommands()
+            return
+        else
+            PsyKeystoneHelper:Print("Unknown command...")
+            PsyKeystoneHelper:printChatCommands()
+            return
+        end
+    end
+end
