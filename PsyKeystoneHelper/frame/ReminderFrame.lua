b3644ed8480f2049f07584a11fb2aaa26f964def
Callum Vernon <callum.vernon@quorumdev.com>
2025-04-28 08:56:14 +0100

    Boilerplate for `ReminderFrame`

---
+++ PsyKeystoneHelper/frame/ReminderFrame.lua
@@ -0,0 +1,64 @@
+local _, ns = ...
+local PsyKeystoneHelper = ns.PsyKeystoneHelper
+local ReminderPopup = { }
+ns.ReminderPopup = ReminderPopup
+
+local firstLoad = true
+
+function ReminderFrame_OnLoad()
+    ReminderFrame:Hide()
+end
+
+function CloseButton_OnClick()
+    ReminderPopup:hide()
+end
+
+function ReminderPopup:maybeInit()
+    -- If this is first load, initialise components
+    if firstLoad then
+        firstLoad = false
+        ReminderPopup:createFrameComponents()
+    end
+end
+
+function ReminderPopup:hide()
+    if ReminderFrame ~= nil and ReminderFrame:IsShown() then
+        ReminderFrame:Hide()
+        ReminderPopup:blankOut()
+    end
+end
+
+function ReminderPopup:showRerollKeystone()
+    ReminderPopup:maybeInit()
+
+    --TODO
+
+    --Show
+    ReminderFrame:Show()
+end
+
+function ReminderPopup:showYourKeystone()
+    ReminderPopup:maybeInit()
+
+    --TODO
+
+    --Show
+    ReminderFrame:Show()
+
+    --Auto close
+    C_Timer.After(10, function()
+        ReminderPopup:hide()
+    end)
+end
+
+function ReminderPopup:createFrameComponents()
+    ReminderFrame.text = PsyKeystoneHelper:createString(ReminderFrame, "GameFontHighlight", 14, "")
+    ReminderFrame.text:SetPoint("TOP", ReminderFrame, "TOP", 0, -20)
+
+    ReminderFrame.keystone = PsyKeystoneHelper:createKeystoneButton(ReminderFrame, 60, 18)
+    ReminderFrame.keystone:SetPoint("CENTER", ReminderFrame, "CENTER", 0, 5)
+end
+
+function ReminderPopup:blankOut()
+    -- TODO
+end
